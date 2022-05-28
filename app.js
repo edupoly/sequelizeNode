5662a00205fc2c76f69a9a8261703ffb4595b1e7
manumitsacademy <manumits.info@gmail.com>
2022-05-28 16:55:43 +0530

    sequelize with transactions

---
+++ app.js
@@ -0,0 +1,8 @@
+var createTutorial = require("./main");
+const tutorial = {
+  title: "pro reactjs",
+  description: "deep dive into component performance",
+  published: false,
+};
+
+createTutorial(tutorial);
--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const db = require("./models/index");
 const Tutorial = db.tutorials;
-const tutorial = {
-  title: "expressjs book",
-  description: "not a bad technology",
-  published: true,
-};
+const sequelize = db.sequelize;
+async function createTutorial(tutorial) {
+  const t = await sequelize.transaction();
+  try {
+    const tutorialStep1 = await Tutorial.create(tutorial, { transaction: t });
+    await t.commit();
+  } catch (error) {
+    await t.rollback();
+  }
+}
 
-// Save Tutorial in the database
-Tutorial.create(tutorial)
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("err::", err);
-  });
+module.exports = createTutorial;
+
+// Tutorial.create(tutorial)
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((err) => {
+//       console.log("err::", err);
+//     });
